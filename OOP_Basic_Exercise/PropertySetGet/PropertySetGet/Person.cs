2f11428a1b7f914a3d370eecfbca48181db2154f
Md. Al Jobaed <tuhincse001@gmail.com>
2017-11-14 17:26:57 +0600

    Add files via upload

---
+++ OOP_Basic_Exercise/PropertySetGet/PropertySetGet/Person.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertySetGet
+{
+    class Person
+    {
+        private string firstName;
+        private string middleName;
+        private string lastName;
+
+        public string FirstName
+        {
+            set
+            {
+                if (value.Length >= 2)
+                {
+                    firstName = value;
+                }
+            }
+            get
+            {
+                return "Md. "+firstName;
+            }
+        }
+        public string MiddleName
+        {
+            set
+            {
+                middleName = value;
+            }
+            get
+            {
+                return middleName;
+            }
+        }
+        public string LastName
+        {
+            set
+            {
+                lastName = value;
+
+            }
+            get
+            {
+                return lastName;
+            }
+        }
+
+        public Person(string firstName,string middleName,string lastName):this(firstName,lastName)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+        }
+        public Person(string firstName,string lastName):this()
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public Person()
+        {
+
+        }
+
+        public string GetFullName()
+        {
+            string fullName = FirstName+" "+MiddleName+" "+LastName;
+            return fullName;
+        }
+    }
+}
---
+++ OOP_Basic_Exercise/PropertySetGet/PropertySetGet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertySetGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your first name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter your middle name: ");
+            string middleName = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            string lastName = Console.ReadLine();
+
+            Person person1 = new Person();
+            person1.FirstName = firstName;
+            person1.MiddleName = middleName;
+            person1.LastName = lastName;
+
+            Console.WriteLine(person1.GetFullName());
+            //Console.WriteLine(person1.FirstName);
+            Console.ReadKey();
+        }
+    }
+}
