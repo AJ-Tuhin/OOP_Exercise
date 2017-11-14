24fd52ec8abd87ba012ea52d0e8c0edb03208c83
Md. Al Jobaed <tuhincse001@gmail.com>
2017-11-14 17:24:20 +0600

    Add files via upload

---
+++ OOP_Basic_Exercise/OOPConstractor/OOPConstractor/OOPConstractor/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConstractor
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string middleName, string lastName) : this(firstName, lastName)
+        {
+            
+            MiddleName = middleName;
+            
+        }
+
+        public Person(string firstName, string lastName):this()
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Person()
+        {
+        }
+
+        public string GetFullName()
+        {
+            string fullName = FirstName+" "+MiddleName+" "+LastName;
+            return fullName;
+        }
+
+        public string GetReverseName(string text)           
+        {
+            char[] cArray = text.ToCharArray();
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+            return reverse;
+        }
+
+    }
+}
---
+++ OOP_Basic_Exercise/OOPConstractor/OOPConstractor/OOPConstractor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConstractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Al","Jobaed","Tuhin");
+            //Console.Write("Enter your First name: ");
+            //person1.FirstName = Console.ReadLine();
+            Console.WriteLine(person1.GetFullName());
+            Console.WriteLine(person1.GetReverseName(person1.GetFullName()));
+            
+            
+            Console.ReadKey();
+
+        }
+    }
+}
